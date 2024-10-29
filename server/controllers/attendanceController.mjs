3c51a9a00e1faa68a58ab4924801045e9c760759
seigfred0 <saysonseigfred@gmail.com>
2024-10-29 23:42:09 +0800

    still working on the database...

--- server/controllers/attendanceController.mjs
+++ server/controllers/attendanceController.mjs
@@ -1,42 +1,91 @@
 import { connect } from "../utils/dbUtils.mjs"
 
+const getHourIn24Format = (timeString) => {
+    const dateObj = new Date(`01/01/2000 ${timeString}`);
+    if (isNaN(dateObj)) {
+        console.error('Invalid date:', timeString);
+        return null;
+    }
+    const hour = dateObj.getHours();
+    return hour;
+}
 
-const checkSession = (session) => {
-    console.log(session)
-    // if session 
+const checkSession = (sessionType, time) => {
+    // const time = date;
+    // console.log(time)
+    const split = time.split(' ');
+    const [hours, minutes, seconds] = split[0].split(':');
+
+    const dayTime = getHourIn24Format(time);
+
+    if (sessionType.toLowerCase() === 'in') {
+        const sessionIn = hours < dayTime && split === 'AM' ? 'amIn' : 'pmIn';
+        console.log('testAM', sessionIn)
+        return sessionIn
+    } else if (sessionType.toLowerCase() === 'out') {
+        const sessionOut = hours < dayTime && split == "PM" ? 'pmIn' : 'pmOut'
+        console.log('testPM', sessionOut)
+        return sessionOut;
+    }
 }
 
 const checkAttendance = async (req, res) => {
+    // problem: this creates a new data everytime. It does not update existing data!
     try {
         const data = req.body;
         const collection = await connect('attendance');
 
+        console.log(data)
+
         for (const studentData of data) {
-            const date = studentData.date;
+            const sessionField = checkSession(studentData.session, studentData.time)
 
-            checkSession(studentData.session)
+            // const result = await collection.updateOne(
+            //     {   
+            //         uid: "attendance_records"
+            //     },
+            //     {
+            //         $push: {
+            //             [`attendance.${studentData.date}`]: {  
+            //                 studentId: studentData.studentId,
+            //                 name: studentData.name,
+            //                 session: {
+            //                     [sessionField]: studentData.time
+            //                 }
+            //             }
+            //         }
+            //     },
+            //     {
+            //         upsert: true  
+            //     }
+            // );
+            
+            
+            // const result = await collection.updateOne(
+            //     { 
+            //         uid: "attendance",
+            //         [`attendance.${date}.studentId`]: studentData.studentId
 
-            const result = await collection.updateOne(
-                { uid: "attendance" },
-                {
-                    $push: {
-                        [`attendance.${date}`]: {
-                            studentId: studentData.studentId,
-                            name: studentData.name,
-                            session: {
-                                amIn: studentData.session,
-                                amOut: studentData.session,
-                                pmIn: studentData.session,
-                                pmOut: studentData.session
-                            }
-                        }
-                    }
-                },
-                { upsert: true }
-            )
+            //     },
+            //     {
+            //         $push: {
+            //             [`attendance.${date}`]: {
+            //                 studentId: studentData.studentId,
+            //                 name: studentData.name,
+            //                 session: {
+            //                     [sessionField]: studentData.time
+            //                 }
+            //             }
+            //             // [`attendance.${date}.$.session.${sessionField}`]: studentData.time,
+            //             // [`attendance.${date}.$.name`]: studentData.name
+            //         }
+            //     },
+            //     { upsert: true }
+            // )
+
+
             console.log(result)
         }
-
         res.send('received something')
     } catch (error) {
         console.log('error',error)   
@@ -44,30 +93,6 @@ const checkAttendance = async (req, res) => {
 }
 
 /*
- for (const record of data) {
-            const date = record.date; // Extract date from each record
-
-            const result = await collection.updateOne(
-                { uid: "attendance" },
-                {
-                    $push: {
-                        [`attendance.${date}`]: {
-                            studentId: record.studentId,
-                            uid: record.uid,
-                            name: record.name,
-                            amIn: record.amIn,
-                            amOut: record.amOut,
-                            pmIn: record.pmIn,
-                            pmOut: record.pmOut
-                        }
-                    }
-                },
-                { upsert: true }
-            );
-
-            console.log(result);
-        }
-
 
 */
 
---
+++ server/models/attendanceModel.mjs
@@ -0,0 +1,4 @@
+
+const checkAttendance = () => {};
+
+const updateAttendance = () => {}
